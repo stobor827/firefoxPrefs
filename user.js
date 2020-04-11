426f2ef9916ad4da99fe5f1fbc05cbfa2e2beba0
Stephen Pierce <pierces@gmail.com>
2020-04-10 22:36:46 -0400

    Add files via upload

---
+++ user.js
@@ -0,0 +1,9 @@
+//disable searching from urlbar
+user_pref( "keyword.enabled", false);
+
+//disable adding www, .com, etc.
+user_pref( "browser.fixup.alternate.enabled", false);
+
+//disable ff75's new oversized and excessively animated urlbar.
+user_pref("browser.urlbar.update1", false);
+user_pref( "browser.urlbar.openViewOnFocus", false);
