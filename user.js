534e4137ddd7006389401a4f7576052b2f4dce2b
Stephen Pierce <pierces@gmail.com>
2020-04-17 12:49:19 -0400

    Update user.js

--- user.js
+++ user.js
@@ -7,3 +7,7 @@ user_pref( "browser.fixup.alternate.enabled", false);
 //disable ff75's new oversized and excessively animated urlbar.
 user_pref("browser.urlbar.update1", false);
 user_pref( "browser.urlbar.openViewOnFocus", false);
+
+//Other ideas:
+//https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion#User_Preferences
+//ui.prefersReducedMotion
