bbd30a3462c8a3e218a719c925845f2fd8dbcc42
Stephen Pierce <pierces@gmail.com>
2020-06-02 21:29:27 -0400

    fixes for new urlbar

--- user.js
+++ user.js
@@ -1,12 +1,18 @@
 //disable searching from urlbar
 user_pref( "keyword.enabled", false);
 
+//disable search suggestions in urlbar
+user_pref("browser.urlbar.suggest.searches", false);
+
 //disable adding www, .com, etc.
 user_pref( "browser.fixup.alternate.enabled", false);
 
 //disable ff75's new oversized and excessively animated urlbar.
 user_pref("browser.urlbar.update1", false);
 user_pref( "browser.urlbar.openViewOnFocus", false);
+
+//enable userChrome.css
+user_pref( "toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 //Other ideas:
 //https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion#User_Preferences
