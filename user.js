7bf4b5a70c1dc52327582acd46ed6c6df9418b7b
Stephen Pierce <pierces@gmail.com>
2022-01-29 17:17:50 -0500

    Update user.js

--- user.js
+++ user.js
@@ -4,6 +4,8 @@ user_pref( "keyword.enabled", false);
 //disable search suggestions in urlbar
 user_pref("browser.urlbar.suggest.searches", false);
 
+user_pref( "browser.search.widget.inNavBar", true);
+
 //disable adding www, .com, etc.
 user_pref( "browser.fixup.alternate.enabled", false);
 
@@ -14,8 +16,9 @@ user_pref( "browser.urlbar.openViewOnFocus", false);
 //enable userChrome.css
 user_pref( "toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-
 user_pref("signon.rememberSignons", false);
+
+
 //Other ideas:
 //https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion#User_Preferences
 //ui.prefersReducedMotion
